a17870c10ea216438eb7467fd9d7b7e667525fc5
Calgalli <calgalli@gmail.com>
2022-01-24 15:10:10 +0000

    add images

--- test1.py
+++ test1.py
@@ -1,2 +1,7 @@
-while True:
-  print('aaaaa')
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+import rospy
+import time
+rospy.init_node('blocky_node', anonymous=True)
+while not rospy.is_shutdown():
+  rospy.sleep(1)
