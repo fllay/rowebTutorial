a196fe0b6dd33aa9bf457d9a47490a4a468de1fe
Calgalli <calgalli@gmail.com>
2022-01-25 02:51:34 +0000

    add images

--- test1.py
+++ test1.py
@@ -1,7 +1,13 @@
+pub = None
+
+
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 import rospy
 import time
 rospy.init_node('blocky_node', anonymous=True)
+pub = rospy.Publisher('chatter', String, queue_size=10)
+
 while not rospy.is_shutdown():
+  pub.publish('HiHi')
   rospy.sleep(1)
